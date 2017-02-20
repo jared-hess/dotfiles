a26398fe0f879db8b0afbc76bc81fee269112075
Jared Hess <jhess@paloaltonetworks.com>
2017-02-20 00:06:17 -0800

    update

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -3,3 +3,4 @@ git clone --bare git@github.com:jared-hess/dotfiles.git $HOME/.dotfiles
 git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout 
 git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME submodule init 
 git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME submodule update 
+git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME config --local status.ShowUntrackedFiles no 
