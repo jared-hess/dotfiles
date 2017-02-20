2ffaecb8656db2252dbb46b2a17200e57977a90f
Jared Hess <jhess@paloaltonetworks.com>
2017-02-19 23:45:30 -0800

    Add install script

---
+++ install_dotfiles.sh
@@ -0,0 +1,5 @@
+alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+git clone --bare git@github.com:jared-hess/dotfiles.git $HOME/.dotfiles
+git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout 
+git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME submodule init 
+git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME submodule update 
