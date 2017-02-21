e039af56c0377285f2991b61c3c87edd33ae31f7
Jared Hess <nobody6.1@gmail.com>
2017-02-21 15:22:57 -0800

    update install script

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -1,6 +1,18 @@
-alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 git clone --bare git@github.com:jared-hess/dotfiles.git $HOME/.dotfiles
-git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout 
-git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME submodule init 
-git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME submodule update 
-git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME config --local status.ShowUntrackedFiles no 
+function config {
+   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
+config checkout
+if [ $? = 0 ]; then
+  echo "Checked out config."
+  else
+    echo "Backing up pre-existing dot files."
+    mkdir -p .dotfiles-backup
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+fi
+config checkout
+config config status.showUntrackedFiles no
+
+config submodule init
+config submodule update
+
