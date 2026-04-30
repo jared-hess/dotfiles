f9ee864417316a0f9d49781b8fed519d3804536d
Jared Hess <jared.hess@alteryx.com>
2026-04-30 14:04:18 -0700

    add vless aliases

--- chezmoi/dot_config/shell/aliases.sh
+++ chezmoi/dot_config/shell/aliases.sh
@@ -36,3 +36,71 @@ if have_cmd nvim; then
   vim() { command nvim "$@"; }
   vi() { command nvim "$@"; }
 fi
+
+vless_cat_fallback() {
+  if [ "$#" -eq 0 ]; then
+    if [ -t 0 ]; then
+      printf 'Missing filename\n' >&2
+      return 1
+    fi
+    cat
+  else
+    cat "$@"
+  fi
+}
+
+find_vim_less_script() {
+  local candidate
+  for candidate in \
+    /usr/share/vim/vim*/macros/less.sh \
+    /usr/share/vim/*/macros/less.sh \
+    /usr/share/vim/*/less.sh
+  do
+    if [ -f "$candidate" ]; then
+      printf '%s\n' "$candidate"
+      return 0
+    fi
+  done
+  return 1
+}
+
+vless() {
+  if have_cmd nvim; then
+    if [ -t 1 ]; then
+      if [ "$#" -eq 0 ]; then
+        if [ -t 0 ]; then
+          printf 'Missing filename\n' >&2
+          return 1
+        fi
+        command nvim --cmd 'let no_plugin_maps = 1' -c 'runtime! scripts/less.vim' -
+      else
+        command nvim --cmd 'let no_plugin_maps = 1' -c 'runtime! scripts/less.vim' "$@"
+      fi
+    else
+      vless_cat_fallback "$@"
+    fi
+    return
+  fi
+
+  if have_cmd vim; then
+    if [ -t 1 ]; then
+      local vim_less_script
+      if vim_less_script="$(find_vim_less_script)"; then
+        command "$vim_less_script" "$@"
+      elif [ "$#" -eq 0 ]; then
+        if [ -t 0 ]; then
+          printf 'Missing filename\n' >&2
+          return 1
+        fi
+        command vim --cmd 'let no_plugin_maps = 1' -c 'runtime! macros/less.vim' -
+      else
+        command vim --cmd 'let no_plugin_maps = 1' -c 'runtime! macros/less.vim' "$@"
+      fi
+    else
+      vless_cat_fallback "$@"
+    fi
+    return
+  fi
+
+  vless_cat_fallback "$@"
+}
